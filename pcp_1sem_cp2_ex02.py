12a6e063809feb3d36466839c7e76243e05aa30d
manciliaotavio0-boop <manciliaotavio0@gmail.com>
2026-04-16 22:05:21 -0300

    questão 2- concluída
    Refactor triangle classification logic and variable assignments.

--- pcp_1sem_cp2_ex02.py
+++ pcp_1sem_cp2_ex02.py
@@ -9,23 +9,27 @@ print(lista)
 
 a=(max(lista))
 lista.remove(a)
-b = lista.pop()
-c = lista.pop()
+b=(max(lista))
+lista.remove(b)
+c = lista[0]
 
 print(a)
 print(b)
 print(c)
 
+forma_triangulo = True 
 if a>=b+c:
   print("Não forma triangulo")
+  forma_triangulo = False
 elif a**2==b**2+c**2:
   print("Triangulo retangulo")
 elif a**2>b**2+c**2:
   print("Triangulo obtusangulo")
 elif a**2<b**2+c**2:
   print("Triangulo acutangulo")
-elif a==b==c:
-  print("Triangulo equilatero")
-elif a==b or a==c or b==c:
-  print("Triangulo isosceles")
 
+if forma_triangulo == True:
+  if a==b and a==c:
+    print("Triangulo equilatero")
+  elif a==b or a==c or b==c:
+    print("Triangulo isosceles")
