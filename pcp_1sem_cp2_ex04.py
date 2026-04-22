d686e728209a6d4c63add018218fbf5b37061599
marcospaulo728 <marcospaulo6357@gmail.com>
2026-04-22 20:35:22 -0300

    Exercicio finalizado!

--- pcp_1sem_cp2_ex04.py
+++ pcp_1sem_cp2_ex04.py
@@ -1,30 +1,35 @@
-nome_func = input("Digite o nome do funcionário: ")
-cargo = int(input("Digite o cargo: (1-Gerente, 2-Analista, 3-Assistente, 4-Estagiário) "))
-salario_base = float(input("Digite o salário base: R$"))
-hora_extra = int(input("Digite o total de horas extras trabalhadas: "))
-falta = int(input("Digite o total de faltas no mês: "))
-bonus = int(input("Recebeu bônus por desempenho? (1-Sim, 2-Não) "))
+def calcular_horas_extras(salario, extras):
+    total_extras = (salario * 0.015) * extras
+    return total_extras
 
-def calcular_horas_extras(salario, total_horas):
-    horas_extras = (salario * 0.015) * total_horas + salario
-    return horas_extras
-
-def calcular_descontos_faltas(a, b):
-    descontos_falta = (a * 0.02) * b
+def calcular_descontos_faltas(salario, faltas):
+    descontos_falta = (salario * 0.02) * faltas
     return descontos_falta
 
-def calcular_bonus(cargoo):
-    if cargoo == 1:
-        valor_bonus = 1000
-    elif cargoo == 2:
-        valor_bonus = 500
-    elif cargoo == 3:
-        valor_bonus = 300
-    elif cargoo == 4:
-        valor_bonus = 100
+def calcular_bonus(cargo, recebe):
+    if recebe != 1:
+        return 0
+
+    if cargo == 1:
+        return 1000
+    elif cargo == 2:
+        return 500
+    elif cargo == 3:
+        return 300
+    elif cargo == 4:
+        return 100
     else:
-        valor_bonus = 0
+        return 0
 
-salario_extra = calcular_horas_extras(salario_base, hora_extra)
+nome_funcionario = input("Digite o nome do funcionário: ")
+nome_cargo = int(input("Digite o cargo: (1-Gerente, 2-Analista, 3-Assistente, 4-Estagiário) "))
+salario_base = float(input("Digite o salário base: R$"))
+horas_extras = int(input("Digite o total de horas extras trabalhadas: "))
+total_faltas = int(input("Digite o total de faltas no mês: "))
+recebe_bonus = int(input("Recebeu bônus por desempenho? (1-Sim, 2-Não) "))
 
-desconto_falta = calcular_descontos_faltas(salario_base, falta)
+print(f"Nome do funcionário: {nome_funcionario}")
+print(f"Salário bruto: R$ {salario_base:.2f}")
+print(f"Total de acréscimos: R$ {calcular_horas_extras(salario_base, horas_extras) + calcular_bonus(nome_cargo, recebe_bonus):.2f}")
+print(f"Total de descontos: R$ {calcular_descontos_faltas(salario_base, total_faltas):.2f}")
+print(f"Salário final: R$ {(salario_base + calcular_horas_extras(salario_base, horas_extras) + calcular_bonus(nome_cargo, recebe_bonus)) - calcular_descontos_faltas(salario_base, total_faltas):.2f}")
